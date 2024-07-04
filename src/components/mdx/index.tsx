ec51d60ae6a570ac582805ff0d73ccf6ce62d9de
jsha <jsha@kei.co.kr>
2024-07-04 14:47:25 +0900

    Fix: MdxComponents 다시 추가

--- src/components/mdx/index.tsx
+++ src/components/mdx/index.tsx
@@ -68,3 +68,53 @@ export const parseToc = (source: string) => {
       return nac
     }, [])
 }
+
+export const MdxComponents = {
+  // a: ExternalLink as any,
+  img: Image as any,
+  h1: (props: any) => (
+    <h1
+      style={{
+        fontSize: '1.6rem',
+        fontWeight: 'bold',
+        marginBottom: '0.8rem',
+      }}
+      {...props}
+    />
+  ),
+  h2: (props: any) => (
+    <h2
+      style={{
+        fontSize: '1.6rem',
+        fontWeight: 'bolder',
+        marginBottom: '0.8rem',
+      }}
+      {...props}
+    />
+  ),
+  h3: (props: any) => (
+    <h3 style={{ fontSize: '1.6rem', marginBottom: '0.8rem' }} {...props} />
+  ),
+
+  h4: (props: any) => (
+    <h4
+      style={{
+        fontSize: '1.6rem',
+        fontWeight: 'bolder',
+        marginBottom: '0.8rem',
+      }}
+      {...props}
+    />
+  ),
+
+  p: (props: any) => (
+    <p
+      style={{ fontSize: '1.6rem', marginBottom: '0.8rem' }}
+      className="text-slate-600"
+      {...props}
+    />
+  ),
+  blockquote: Callout,
+  Callout,
+  pre: Pre as any,
+}
